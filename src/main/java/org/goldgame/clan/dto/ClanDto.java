9a9359bac1a8e43f7f4c986220cc7f03d8ab1c85
Alexander <71982238+AlSl94@users.noreply.github.com>
2022-10-05 12:38:38 +0300

    feat: postman tests

--- src/main/java/org/goldgame/clan/dto/ClanDto.java
+++ src/main/java/org/goldgame/clan/dto/ClanDto.java
@@ -2,14 +2,21 @@ package org.goldgame.clan.dto;
 
 import lombok.*;
 import org.goldgame.person.dto.PersonDto;
+import org.goldgame.utilities.Create;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder(toBuilder = true)
-@Getter @Setter @ToString
+@Getter
+@Setter
+@ToString
 public class ClanDto {
+    @NotNull(groups = Create.class)
+    @NotBlank(groups = Create.class)
     private String name;
     private Long gold;
     private List<PersonDto> population;
